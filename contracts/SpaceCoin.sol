2df740995b4b37ccf4df2f39eb912a8754948eb5
Gabriel <gabrieljim641@gmail.com>
2021-09-28 14:05:00 -0400

    Contract and tests

---
+++ contracts/SpaceCoin.sol
@@ -0,0 +1,125 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "hardhat/console.sol";
+
+contract SpaceCoin is ERC20 {
+    enum Phase {
+        SEED,
+        GENERAL,
+        OPEN
+    }
+    Phase public currentPhase = Phase.SEED;
+
+    uint256 public MAX_SUPPLY;
+    uint256 public constant TAX = 2; // 0.02, 2% of the tx;
+    uint256 public totalContributed;
+    bool isContractPaused = false;
+    address public owner;
+    address payable public treasuryWallet;
+
+    mapping(address => uint256) public balancesToClaim;
+    mapping(address => uint256) public contributionsOf;
+    mapping(address => bool) public isWhitelisted;
+
+    constructor(address payable treasury) ERC20("Space Coin", "SC") {
+        MAX_SUPPLY = 500000 * 10**decimals();
+        _mint(address(this), MAX_SUPPLY);
+        owner = msg.sender;
+        treasuryWallet = treasury;
+        isWhitelisted[owner] = true;
+    }
+
+    modifier ownerOnly() {
+        require(msg.sender == owner, "OWNER_ONLY");
+        _;
+    }
+
+    modifier isPaused() {
+        require(!isContractPaused, "CONTRACT_PAUSED");
+        _;
+    }
+
+    function contribute() external payable isPaused {
+        require(canUserContribute(msg.sender), "NOT_ALLOWED");
+        require(
+            contributionsOf[msg.sender] + msg.value <= getIndividualLimit(),
+            "ABOVE_MAX_INDIVIDUAL_CONTRIBUTION"
+        );
+        require(
+            totalContributed + msg.value <= getPhaseLimit(),
+            "ABOVE_MAX_CONTRIBUTION"
+        );
+
+        uint256 amountToTake = (TAX * msg.value) / 100;
+        uint256 userAmount = msg.value - amountToTake;
+
+        /*
+         * The spec says that the exchange rate must be 5 tokens to 1 ether, so give the sender 5 times the ether they sent
+         */
+        balancesToClaim[msg.sender] += userAmount * 5;
+        contributionsOf[msg.sender] += userAmount;
+        totalContributed += userAmount;
+
+        treasuryWallet.transfer(amountToTake);
+    }
+
+    function getIndividualLimit() private view returns (uint256) {
+        if (currentPhase == Phase.SEED) {
+            return 1500 ether;
+        } else if (currentPhase == Phase.GENERAL) {
+            return 1000 ether;
+        } else {
+            // No limit, just make it the same value so the tx goes through
+            return msg.value;
+        }
+    }
+
+    function getPhaseLimit() private view returns (uint256 limit) {
+        if (currentPhase == Phase.SEED) {
+            limit = 15000 ether;
+        } else if (
+            currentPhase == Phase.GENERAL || currentPhase == Phase.OPEN
+        ) {
+            limit = 30000 ether;
+        }
+    }
+
+    function claimTokens() external isPaused {
+        require(currentPhase == Phase.OPEN, "NOT_LAST_PHASE");
+        require(balancesToClaim[msg.sender] > 0, "NO_AVAILABLE_FUNDS");
+        uint256 tokensToClaim = balancesToClaim[msg.sender];
+        balancesToClaim[msg.sender] = 0;
+
+        _transfer(address(this), msg.sender, tokensToClaim);
+    }
+
+    function advancePhase() external ownerOnly {
+        require(currentPhase != Phase.OPEN, "LAST_PHASE");
+        currentPhase = Phase(uint256(currentPhase) + 1);
+    }
+
+    function togglePauseContract() external ownerOnly {
+        isContractPaused = !isContractPaused;
+    }
+
+    function addToWhitelist(address account) external ownerOnly {
+        isWhitelisted[account] = true;
+    }
+
+    function canUserContribute(address account) public view returns (bool) {
+        return isWhitelisted[account] || currentPhase != Phase.SEED;
+    }
+
+    function mint(address account, uint256 amount) external ownerOnly {
+        require(account != address(0), "ERC20: mint to the zero address");
+        require(totalSupply() + amount <= MAX_SUPPLY, "ABOVE_MAX_SUPPLY");
+
+        _mint(account, amount * 10**decimals());
+    }
+
+    function burn(address account, uint256 amount) external ownerOnly {
+        _burn(account, amount * 10**decimals());
+    }
+}
---
+++ test/SpaceCoin.test.js
@@ -0,0 +1,276 @@
+const { expect } = require("chai");
+const { parseEther } = require("ethers/lib/utils");
+const { ethers } = require("hardhat");
+
+describe("Space Coin Contract", function () {
+  let owner, addr1, addr2, addrs, spaceCoin, treasury;
+
+  beforeEach(async () => {
+    [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
+    const SpaceCoin = await ethers.getContractFactory("SpaceCoin");
+    treasury = addrs[35];
+    spaceCoin = await SpaceCoin.deploy(treasury.address);
+  });
+
+  describe("Deployment", () => {
+    it("Creates 500,000 available tokens", async () => {
+      const supply = await spaceCoin.totalSupply();
+      expect(supply).to.be.equal("500000000000000000000000");
+    });
+
+    it("Assigns the owner correctly", async () => {
+      const currentOwner = await spaceCoin.owner();
+      expect(currentOwner).to.be.equal(owner.address);
+    });
+
+    it("Assigns the symbol of the token correctly", async () => {
+      const symbol = await spaceCoin.symbol();
+      expect(symbol).to.be.equal("SC");
+    });
+
+    it("Assigns the symbol of the token correctly", async () => {
+      const name = await spaceCoin.name();
+      expect(name).to.be.equal("Space Coin");
+    });
+
+    it("Adds owner to whitelist", async () => {
+      const isOwnerWhitelisted = await spaceCoin.isWhitelisted(owner.address);
+      expect(isOwnerWhitelisted).to.be.true;
+    });
+  });
+
+  describe("Only owner", () => {
+    it("Only owner can advance phase", async () => {
+      await expect(spaceCoin.connect(addr1).advancePhase()).to.be.revertedWith(
+        "OWNER_ONLY"
+      );
+    });
+
+    it("Only owner can pause contract", async () => {
+      await expect(
+        spaceCoin.connect(addr1).togglePauseContract()
+      ).to.be.revertedWith("OWNER_ONLY");
+    });
+
+    it("Only owner can mint", async () => {
+      await expect(
+        spaceCoin.connect(addr1).mint(addr1.address, 1000)
+      ).to.be.revertedWith("OWNER_ONLY");
+    });
+
+    it("Only owner can burn", async () => {
+      await expect(
+        spaceCoin.connect(addr1).burn(spaceCoin.address, 1000)
+      ).to.be.revertedWith("OWNER_ONLY");
+    });
+
+    it("Only owner can add an address to whitelist", async () => {
+      await expect(
+        spaceCoin.connect(addr1).addToWhitelist(addr1.address)
+      ).to.be.revertedWith("OWNER_ONLY");
+    });
+  });
+
+  describe("Contributing", () => {
+    it("Assigns 5 times the value contributed", async () => {
+      await spaceCoin.contribute({ value: parseEther("0.25") }); // Minus 2% = 0.245
+
+      const assignedBalance = await spaceCoin.balancesToClaim(owner.address);
+      expect(assignedBalance).to.be.equal(parseEther("1.225")); // 0.245 * 5 = 1.225 tokens
+    });
+
+    it("Sends 2% of the transaction to the treasury wallet", async () => {
+      await expect(
+        await spaceCoin.contribute({ value: parseEther("1") })
+      ).to.changeEtherBalance(treasury, parseEther("0.02"));
+    });
+  });
+
+  describe("Minting and burning", () => {
+    it("Reverts mint if it would go above the maximum allowed supply", async () => {
+      await expect(spaceCoin.mint(owner.address, 100)).to.be.revertedWith(
+        "ABOVE_MAX_SUPPLY"
+      );
+    });
+
+    it("Burns correctly", async () => {
+      await spaceCoin.burn(spaceCoin.address, 100);
+
+      const newSupply = await spaceCoin.totalSupply();
+      expect(newSupply).to.be.equal(parseEther("499900"));
+    });
+
+    it("Mints and assigns correctly", async () => {
+      await spaceCoin.burn(spaceCoin.address, 100);
+
+      const supply = await spaceCoin.totalSupply();
+      expect(supply).to.be.equal(parseEther("499900"));
+
+      await spaceCoin.mint(owner.address, 100);
+
+      const newSupply = await spaceCoin.totalSupply();
+      const newOwnerBalance = await spaceCoin.balanceOf(owner.address);
+
+      expect(newSupply).to.be.equal(parseEther("500000"));
+      expect(newOwnerBalance).to.be.equal(parseEther("100"));
+    });
+  });
+
+  describe("Pausing", () => {
+    it("Pauses on SEED", async () => {
+      await spaceCoin.contribute({ value: parseEther("0.25") });
+
+      await spaceCoin.togglePauseContract();
+
+      await expect(
+        spaceCoin.contribute({ value: parseEther("0.25") })
+      ).to.be.revertedWith("CONTRACT_PAUSED");
+
+      await expect(spaceCoin.claimTokens()).to.be.revertedWith(
+        "CONTRACT_PAUSED"
+      );
+    });
+
+    it("Pauses on GENERAL", async () => {
+      await spaceCoin.advancePhase();
+      await spaceCoin.contribute({ value: parseEther("0.25") });
+
+      await spaceCoin.togglePauseContract();
+
+      await expect(
+        spaceCoin.contribute({ value: parseEther("0.25") })
+      ).to.be.revertedWith("CONTRACT_PAUSED");
+
+      await expect(spaceCoin.claimTokens()).to.be.revertedWith(
+        "CONTRACT_PAUSED"
+      );
+    });
+
+    it("Pauses on OPEN", async () => {
+      await spaceCoin.advancePhase();
+      await spaceCoin.advancePhase();
+
+      await spaceCoin.contribute({ value: parseEther("0.25") });
+
+      await spaceCoin.togglePauseContract();
+
+      await expect(
+        spaceCoin.contribute({ value: parseEther("0.25") })
+      ).to.be.revertedWith("CONTRACT_PAUSED");
+
+      await expect(spaceCoin.claimTokens()).to.be.revertedWith(
+        "CONTRACT_PAUSED"
+      );
+    });
+  });
+
+  describe("Phases", () => {
+    it("Increases phases correctly", async () => {
+      await spaceCoin.advancePhase();
+      await spaceCoin.advancePhase();
+
+      const phase = await spaceCoin.currentPhase();
+      expect(phase).to.be.equal(2);
+    });
+
+    it("Reverts increasing phase if it's the last one", async () => {
+      await spaceCoin.advancePhase();
+      await spaceCoin.advancePhase();
+
+      await expect(spaceCoin.advancePhase()).to.be.revertedWith("LAST_PHASE");
+    });
+
+    describe("SEED phase", () => {
+      it("Reverts if individual total above 1500 ether", async () => {
+        await spaceCoin.contribute({ value: parseEther("1400") });
+        await expect(
+          spaceCoin.contribute({ value: parseEther("200") })
+        ).to.be.revertedWith("ABOVE_MAX_INDIVIDUAL_CONTRIBUTION");
+      });
+
+      it("Reverts if total contributions above 15,000 ether", async () => {
+        //Donate maximum from 10 different address
+        for (let i = 0; i < 10; i++) {
+          await spaceCoin.addToWhitelist(addrs[i].address);
+          await spaceCoin
+            .connect(addrs[i])
+            .contribute({ value: parseEther("1500") });
+        }
+
+        //Current total contributions: 14,700
+        await spaceCoin.addToWhitelist(addrs[10].address);
+        await expect(
+          spaceCoin.connect(addrs[10]).contribute({ value: parseEther("301") })
+        ).to.be.revertedWith("ABOVE_MAX_CONTRIBUTION");
+      });
+    });
+
+    describe("GENERAL phase", () => {
+      beforeEach(async () => {
+        await spaceCoin.advancePhase();
+      });
+
+      it("Reverts if individual total above 1000 ether", async () => {
+        await spaceCoin.contribute({ value: parseEther("1000") }); // Contributed: 980
+        await expect(
+          spaceCoin.contribute({ value: parseEther("21") })
+        ).to.be.revertedWith("ABOVE_MAX_INDIVIDUAL_CONTRIBUTION");
+      });
+
+      it("Reverts if total contributions above 30,000 ether", async () => {
+        //Donate maximum from 30 different address
+        for (let i = 0; i < 30; i++) {
+          await spaceCoin
+            .connect(addrs[i])
+            .contribute({ value: parseEther("1000") });
+        }
+
+        //Current total contributions: 29,400
+        await expect(
+          spaceCoin.connect(addrs[30]).contribute({ value: parseEther("700") })
+        ).to.be.revertedWith("ABOVE_MAX_CONTRIBUTION");
+      });
+    });
+  });
+
+  describe("OPEN phase", () => {
+    it("Reverts if total contributions above 30,000 ether", async () => {
+      await spaceCoin.advancePhase();
+      await spaceCoin.advancePhase();
+
+      //Donate maximum from 30 different address
+      for (let i = 0; i < 30; i++) {
+        await spaceCoin
+          .connect(addrs[i])
+          .contribute({ value: parseEther("1000") });
+      }
+
+      //Current total contributions: 29,400
+      await expect(
+        spaceCoin.connect(addrs[30]).contribute({ value: parseEther("700") })
+      ).to.be.revertedWith("ABOVE_MAX_CONTRIBUTION");
+    });
+
+    it("Reverts if claiming not allowed yet", async () => {
+      await spaceCoin.contribute({ value: parseEther("10") });
+
+      await expect(spaceCoin.claimTokens()).to.be.revertedWith(
+        "NOT_LAST_PHASE"
+      );
+    });
+
+    it("Allows user to claim tokens assigned to them", async () => {
+      await spaceCoin.advancePhase();
+      await spaceCoin.connect(addr1).contribute({ value: parseEther("0.5") });
+      await spaceCoin.advancePhase();
+
+      await spaceCoin.connect(addr1).claimTokens();
+
+      const balanceOf = await spaceCoin.balanceOf(addr1.address);
+
+      // Contributed 0.5 - 2% = 0.49
+      // 0.49 * 5 = 2.45 tokens
+      expect(balanceOf).to.be.equal(parseEther("2.45"));
+    });
+  });
+});
