8ed976ff712728e96991cc558dccb1cdcfe713bb
Gabriel <gabrieljim641@gmail.com>
2021-10-12 15:11:44 -0400

    Added tabs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,9 @@
 import "./App.css";
 import SpaceCoinInfo from "./components/SpaceCoinInfo/SpaceCoinInfo";
 import "react-toastify/dist/ReactToastify.css";
+import "react-tabs/style/react-tabs.css";
+
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 
 import useContract from "./utils/hooks/useContract";
 
@@ -11,7 +14,19 @@ const App = () => {
     <div className="App">
       <h1>Space Coin</h1>
       {contract ? (
-        <SpaceCoinInfo />
+        <Tabs>
+          <TabList>
+            <Tab>Space Coin Info</Tab>
+            <Tab>Manage Liquidity</Tab>
+            <Tab>Trading</Tab>
+          </TabList>
+
+          <TabPanel>
+            <SpaceCoinInfo />
+          </TabPanel>
+          <TabPanel>Tab 1</TabPanel>
+          <TabPanel>Tab 2</TabPanel>
+        </Tabs>
       ) : (
         "Please connect your wallet to use the dapp!"
       )}
