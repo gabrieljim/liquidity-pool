26ed8d91b9496e476a6c5ae192ba62864653a02b
Gabriel <gabrieljim641@gmail.com>
2021-10-12 16:06:16 -0400

    Deploy script deploys all 4 contracts and assigns addresses

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -8,11 +8,35 @@ async function main() {
       ? "0x2f4f06d218e426344cfe1a83d53dad806994d325"
       : process.env.TREASURY_WALLET
   );
-
   console.log("Deploying SpaceCoin");
   await spaceCoin.deployed();
-
   console.log("Space Coin deployed to:", spaceCoin.address);
+
+  const LiquidityPool = await hre.ethers.getContractFactory("LiquidityPool");
+  const liquidityPool = await LiquidityPool.deploy();
+  console.log("Deploying LiquidityPool");
+  await liquidityPool.deployed();
+  console.log("Liquidity Pool deployed to:", liquidityPool.address);
+
+  const LPT = await hre.ethers.getContractFactory("LPT");
+  const lpt = await LPT.deploy(liquidityPool.address);
+  console.log("Deploying LPT");
+  await lpt.deployed();
+  console.log("LPT deployed to:", lpt.address);
+
+  await liquidityPool.setSpaceCoinAddress(spaceCoin.address);
+  await liquidityPool.setLPTAddress(lpt.address);
+
+  const SpaceRouter = await hre.ethers.getContractFactory("SpaceRouter");
+  const spaceRouter = await SpaceRouter.deploy(
+    liquidityPool.address,
+    spaceCoin.address
+  );
+  console.log("Deploying SpaceRouter");
+  await spaceRouter.deployed();
+  console.log("Space Router deployed to:", spaceRouter.address);
+
+  await spaceCoin.setRouterAddress(spaceRouter.address);
 }
 
 main()
